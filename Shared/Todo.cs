84c3db5121afa7c3c3f46d1f2c67116dc349f19a
LucaQueiroz <luca-queiroz@univem.edu.br>
2020-09-03 19:47:19 -0300

    first commit

---
+++ Shared/Todo.cs
@@ -0,0 +1,12 @@
+using System; 
+namespace BlazorAplication.Shared
+{
+    public class todo
+    {
+        public string Titulo { get; set; }
+        public bool Feito { get; set; }
+        public DateTime Inicio {get;set;}
+        public DateTime Fim {get;set;}
+
+    }
+}
